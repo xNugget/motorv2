1150ca720588c1d13b1403660127dfe89a610d2f
xNugget <llberkayyilmazll@hotmail.com>
2022-02-17 14:22:50 +0300

    update

---
+++ motor/Assets/Scripts/GroundTile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+    void Update()
+    {
+        
+    }
+}
